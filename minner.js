11cde36642d88a645d20bf8ade68baf90147be40
ruzyvell <ruzyvell@gmail.com>
2016-11-08 11:55:33 -0200

    Update minner.js

--- minner.js
+++ minner.js
@@ -4,9 +4,7 @@ var fs = require('fs');
 
 request('http://www.cnpq.br/web/guest/licitacoes', function(err, res, body){
 	if (err) console.log('Erro: ' + err);
-
 	var $ = cheerio.load(body);
-
 	$('.formLicit tr').each(function() {
 		var title = $(this).find('.titLicitacao a').text().trim();
 		var title = $(this).find('.cont_licitacoes a').text().trim();
@@ -14,7 +12,6 @@ request('http://www.cnpq.br/web/guest/licitacoes', function(err, res, body){
 		var title = $(this).find('.outro-doc-link a').text().trim();
 
 		console.log(`Titulo: ` + titLicitacao);
-
 		fs.appendFile(`dados.txt`)
 	});
 
