9306e80d1ff1325e8cfcf684c0bd582440de26e7
ruzyvell <ruzyvell@gmail.com>
2016-11-08 09:23:24 -0200

    Add files via upload

---
+++ minner.js
@@ -0,0 +1,21 @@
+var request = require('request');
+var cheerio = require('cheerio');
+var fs = require('fs');
+
+request('http://www.cnpq.br/web/guest/licitacoes', function(err, res, body){
+	if (err) console.log('Erro: ' + err);
+
+	var $ = cheerio.load(body);
+
+	$('.formLicit tr').each(function() {
+		var title = $(this).find('.titLicitacao a').text().trim();
+		var title = $(this).find('.cont_licitacoes a').text().trim();
+		var title = $(this).find('.data_licitacao a').text().trim();
+		var title = $(this).find('.outro-doc-link a').text().trim();
+
+		console.log(`Titulo: ` + title);
+
+		fs.appendFile(`dados.txt`)
+	});
+
+}
